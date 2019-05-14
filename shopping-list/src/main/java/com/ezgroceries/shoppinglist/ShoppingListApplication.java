3c4cb195dceca2579eca315d01e9043db2474500
Mathias Vanhove <mathias.vanhove@kbc.be>
2019-05-14 21:33:51 +0200

    lab 04

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/ShoppingListApplication.java
@@ -2,10 +2,12 @@ package com.ezgroceries.shoppinglist;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
+@EnableFeignClients
 public class ShoppingListApplication {
 
     public static void main(String[] args) {
